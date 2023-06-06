2c3921222c2fdc80797a936efc0fe63bdca704f2
Vytautas Jonkus <vytaut.jonk@gmail.com>
2023-06-06 21:55:58 +0300

    1  test fix, deleted incomplete test

--- test/specs/hg_testing.spec.js
+++ test/specs/hg_testing.spec.js
@@ -4,30 +4,22 @@ describe('Practice and learning to create automation tests', () => {
     before(async () => {
         await browser.url ('/login');
         loginPage.loginForm('vyckatest1@gmail.com', 'Testing123');
-   
-    // });
-    // it('Test 1 - Achievement "Honeygain Budddy" should be disable', async() => {
-    //     //New test, achievement must be disable when Progress 0/1 
-    //     // let honeyBuddyButton = $("//h3[.='Honeygain Buddy']");
-    //     // await honeyBuddyButton.click();
 
     });
-    it('Test 2 - Should be Information alert after enabling JumpTask mode ', async() => {
+    it('Test 1 - Should be Information alert after enabling JumpTask mode ', async() => {
         let jumptaskAlert = $("//div[@class='Toastify__toast-container Toastify__toast-container--bottom-right']");
         let jumptaskAlertText = $("//div[@id='root']/div[1]/div[@class='Toastify']")
 
         loginPage.enableJtModeButton.click();
         await jumptaskAlert.waitForDisplayed();
         await expect(jumptaskAlertText).toHaveTextContaining("now earning in the JumpTask mode!");
-        
-    // });
-    // it('Test 4 ', async() => {
-    //     loginPage.historyButton.click();
-        
-        // waitUntil(browser.toHaveUrl('https://dashboard.honeygain.com/transactions'));
-        // await expect(browser).toHaveUrl("https://dashboard.honeygain.com/transactions");
+
+        await loginPage.enableHgModeButton.click();
+        await jumptaskAlert.waitForDisplayed();
+        await await expect(jumptaskAlertText).toHaveTextContaining("now earning in the Honeygain mode!");
+
     });
-    it('Test 5 - Should be a success alert after clicking to copy referral code', async() => {
+    it('Test 2 - Should be a success alert after clicking to copy referral code', async() => {
         let referralsCopyButton = $("//div//button[.='Copy']");
         let copiedToClipboardAllert = $("div[type='success']");
         
@@ -35,11 +27,6 @@ describe('Practice and learning to create automation tests', () => {
         await referralsCopyButton.click();
         await copiedToClipboardAllert.waitForDisplayed();
         await expect(copiedToClipboardAllert).toHaveAttributeContaining("type", "success");
-        
-    // });
-    // it('Test 6  ', async() => {
-    //     // loginPage.achievementButton.click();
-        
 
     });
     
