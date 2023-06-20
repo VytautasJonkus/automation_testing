91ca1137b1df8704edb27e6b5293e357289d37d9
Vytautas Jonkus <vytaut.jonk@gmail.com>
2023-06-20 21:00:17 +0300

    Await edited

--- test/specs/hg_testing.spec.js
+++ test/specs/hg_testing.spec.js
@@ -10,20 +10,20 @@ describe('Practice and learning to create automation tests', () => {
         let alertJt = $(".toast-container:nth-of-type(1) [class='Spacing--1nc0q8d smAke']");
         let alertJtText = $("//div[@id='root']/div[1]/div[@class='Toastify']")
 
-        loginPage.buttonWalletModeJT.click();
+        await loginPage.buttonWalletModeJT.click();
         await alertJt.waitForDisplayed();
         await expect(alertJtText).toHaveTextContaining("now earning in the JumpTask mode!");
 
-        loginPage.buttonWalletModeHG.click();
+        await loginPage.buttonWalletModeHG.click();
         await alertJt.waitForDisplayed();
-        await await expect(alertJtText).toHaveTextContaining("now earning in the Honeygain mode!");
+        await expect(alertJtText).toHaveTextContaining("now earning in the Honeygain mode!");
 
     });
     it('Test 2 - Should be a success alert after clicking to copy referral code', async() => {
         let buttonRefCopy= $("//div//button[.='Copy']");
         let alertCopy = $("div[type='success']");
         
-        loginPage.buttonReferrals.click();
+        await loginPage.buttonReferrals.click();
         await buttonRefCopy.click();
         await alertCopy.waitForDisplayed();
         await expect(alertCopy).toHaveAttributeContaining("type", "success");
